ab7f5eb50125d2d0ec91331a8ac9ccf50707f91c
sobirovo6 <matrizaevaz@gmail.com>
2024-10-11 15:41:11 +0500

    .

---
+++ dars2.py
@@ -0,0 +1,10 @@
+def sonni_hisobla():
+    son = int(input("Sonni kiriting: "))
+
+    kvadrat = son ** 2
+    kub = son ** 3
+
+    print(f"{son} ning kvadrati: {kvadrat}")
+    print(f"{son} ning kubi: {kub}")
+
+sonni_hisobla()
