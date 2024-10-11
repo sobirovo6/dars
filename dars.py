2719bbf46104fa3261a8cf4bbd1cd7d0ece5d28c
sobirovo6 <matrizaevaz@gmail.com>
2024-10-11 15:31:15 +0500

    dars

---
+++ dars.py
@@ -0,0 +1,6 @@
+def yosh_hisobla (j_yil):
+    ism = input("ismingizni kiriting")
+    yosh = int(input("yoshingizni kiriting"))
+    print(f"{ism} siz {j_yil-yosh}- yilda tugilgansiz")
+
+yosh_hisobla(2024)
