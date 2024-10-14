382b35559cd59c7d9c49974946d4bda00eaf21d0
sobirovo6 <matrizaevaz@gmail.com>
2024-10-14 16:12:37 +0500

    .

--- dars.py
+++ dars.py
@@ -1,6 +1,51 @@
-def yosh_hisobla (j_yil):
-    ism = input("ismingizni kiriting")
-    yosh = int(input("yoshingizni kiriting"))
-    print(f"{ism} siz {j_yil-yosh}- yilda tugilgansiz")
+# #1
+# def yosh_hisobla (j_yil):
+#     ism = input("ismingizni kiriting")
+#     yosh = int(input("yoshingizni kiriting"))
+#     print(f"{ism} siz {j_yil-yosh}- yilda tugilgansiz")
+#
+# yosh_hisobla(2024)
+#
+# #2
+# def sonni_hisobla():
+#     son = int(input("Sonni kiriting: "))
+#
+#     kvadrat = son ** 2
+#     kub = son ** 3
+#
+#     print(f"{son} ning kvadrati: {kvadrat}")
+#     print(f"{son} ning kubi: {kub}")
+#
+# sonni_hisobla()
 
-yosh_hisobla(2024)
+# def avto_info(kompamiya, model, rangi, karobka, yili, narhi=None):
+#     avto = {
+#         "kompaniya": kompamiya,
+#         'model': model,
+#         'rangi': rangi,
+#         'karopka': karobka,
+#         'yili': yili,
+#         'narhi': narhi
+#     }
+#     return avto
+#
+# avto1 = avto_info('GM', 'malibu' ,'qora', 'avtamat', 2018)
+# avto2 = avto_info('GM', 'gentra' ,'oq', 'mexanika', 2017, 10000)
+# avtolar = [avto1,avto2]
+# print('online bozordagi mavjud avtomashinalar')
+# for avto in avtolar:
+#     if avto['narhi']:
+#         narhi = avto['narhi']
+#     else:
+#         narhi = 'nomalum'
+#         print(f"{avto['rangi']} {avto['model']} narhi {narhi}")
+
+# def oraliq(min,max):
+#     sonlar = []
+#     while min<max:
+#         sonlar.append(min)
+#         min +=1
+#     return sonlar
+#
+# print(oraliq(5,1000))
+
