9b7465f25009b397fa98b11cb47a1d356d807d31
sobirovo6 <matrizaevaz@gmail.com>
2024-10-21 16:20:48 +0500

    .

---
+++ dars 1.py
@@ -0,0 +1,19 @@
+ismlar2 = ["sardor", "donyor", "behruz" , "nurhon" , "temur"]
+for mehmon in ismlar2:
+    print(f"salom  {mehmon}")
+
+
+
+
+toq_sonlar = list(range(11,100,2))
+for n in toq_sonlar:
+    print(f"{n} - kubi ={n**3}")
+
+
+
+dostlar = []
+print("5ta eng sevimli kinoyingizni kiriting ")
+for n in range(5):
+    dostlar.append(input(f"{n+1}- kino kiriting "))
+
+print(dostlar)
